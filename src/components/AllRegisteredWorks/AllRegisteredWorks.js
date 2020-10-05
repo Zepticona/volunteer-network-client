b5b9647c6475e43c21c08119415b3a24b7f72154
Zepticona <67518532+Zepticona@users.noreply.github.com>
2020-10-05 18:46:16 +0600

    ready for deploy

--- src/components/AllRegisteredWorks/AllRegisteredWorks.js
+++ src/components/AllRegisteredWorks/AllRegisteredWorks.js
@@ -21,10 +21,12 @@ const AllRegisteredWorks = () => {
         <Container>
             <NavigationBar showEmail={true}></NavigationBar>
             <p>Please wait while we get your registered volunteering jobs.</p>
-            <p style={{margninBottom: '2px'}}>If you are having trouble getting the required informations</p>
+            <p style={{margninBottom: '2px'}}>If you are having trouble getting the required informations follow the instructions bellow:</p>
+            <ul style={{listStyle: 'none'}}>
             <li style={{color: 'red'}}>Make sure that you are logged in.</li>
-            <li> style={{color: 'red'}}Click "Registered Works" on the menu.</li>
+            <li style={{color: 'red'}}>Click "Registered Works" on the menu.</li>
             <li style={{marginBottom: '20px', color: 'red'}}>Make sure that you've added any task to your account.</li>
+            </ul>
             <Row>
                 {
                     registeredWorks.map( (el, i) => <Col key={i} xs={12} md={6}><RegisteredWorks workInfo={el}></RegisteredWorks></Col>)
